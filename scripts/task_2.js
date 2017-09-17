8c590c92343876840584946d84b53af09921d342
Andrej Rushnitski <a.rushnitski@gmail.com>
2017-09-17 21:57:44 +0300

    Добавлена работа визуализации задачи №2

--- scripts/task_2.js
+++ scripts/task_2.js
@@ -30,9 +30,25 @@ Array.prototype.suffleNewArray = function () {
     function randomElem(array) {
         return elem = array.splice(Math.floor(Math.random() * array.length), 1);
     }
-    for (var index = 0; index < arr.length;) {
+    for (let index = 0; index < arr.length;) {
         randomElem(arr);
         res.push(elem[0]);
     }
     return res
 };
+
+let suffleBtn = document.getElementsByName('suffleBtn')[0];
+suffleBtn.addEventListener("click", setResult);
+let suffleArr = document.getElementsByName('suffleArr')[0];
+let res = document.getElementsByClassName('result')[0];
+res.appendChild(span);
+
+/**
+ * Метод преобразует введеную строку в массив и 
+ * заполняет значение вставляемого результата в созданный элемент
+ */
+function setResult() {
+    let valueArr = suffleArr.value.split(",");
+    span.innerHTML = valueArr.suffle();
+}
+
